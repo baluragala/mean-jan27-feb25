3a3bbfcbb79d62b566a7034ca7b8b5ffabfdeb36
Bala Krishna Ragala <baluragala@gmail.com>
2018-02-25 21:45:27 +0530

    DI

--- e-store/src/app/app.component.ts
+++ e-store/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component, ViewChild, ElementRef } from "@angular/core";
+import { ProductService } from "./products/product.service";
 
 @Component({
   selector: "super-app",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  styleUrls: ["./app.component.css"],
+  providers: [{ provide: ProductService, useClass: ProductService }]
 })
 export class AppComponent {
   title = "app";
