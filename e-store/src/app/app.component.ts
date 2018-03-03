d088797198eaae50d462b2d6807c2c3889c3a039
Bala Krishna Ragala <baluragala@gmail.com>
2018-03-03 20:39:51 +0530

    rxjs

--- e-store/src/app/app.component.ts
+++ e-store/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild, ElementRef } from "@angular/core";
 import { ProductService } from "./products/product.service";
+import { Observable } from "rxjs";
 
 @Component({
   selector: "super-app",
@@ -9,9 +10,43 @@ import { ProductService } from "./products/product.service";
 })
 export class AppComponent {
   title = "app";
-  @ViewChild("ftr") footer: ElementRef;
+  //   @ViewChild("ftr") footer: ElementRef;
 
-  ngAfterViewInit() {
-    this.footer.nativeElement.innerText = "this is set from comp";
+  //   ngAfterViewInit() {
+  //     this.footer.nativeElement.innerText = "this is set from comp";
+  //   }
+  // }
+
+  fromArrayResult = "";
+  fromObjectResult;
+  fromRangeResult = "";
+  fromIntervalResult = "";
+  fromPromiseResult = "";
+  mapOperatorResult = "";
+  filterOperatorResult = "";
+  flatMapOperatorResult = "";
+  forkJoinOperatorResult;
+
+  constructor() {
+    // var source$ = Observable.create(function(observer) {
+    //   observer.next(20);
+    //   observer.complete();
+    // });
+    // source$.subscribe(
+    //   function next(d) {
+    //     console.log(d);
+    //   },
+    //   function error(e) {
+    //     console.error(e);
+    //   },
+    //   function complete() {
+    //     console.log("completed");
+    //   }
+    // );
+
+    let s$ = Observable.interval(1000)
+      .map(v => v * 20)
+      .filter(v => v % 30 === 0)
+      .subscribe(v => console.log(v));
   }
 }
